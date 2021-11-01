39f1aa12d7b0a071007cfdf5301e05880836140b
faithm2020 <91081126+faithm2020@users.noreply.github.com>
2021-11-01 23:32:06 +0400

    JS and INDEX

--- game.js
+++ game.js
@@ -6,6 +6,7 @@ function Bear() {
   this.y = this.htmlElement.offsetTop;
 
   this.move = function (xDir, yDir) {
+    this.fitBounds(); //we add this instruction to keep bear within board
     this.x += this.dBear * xDir;
     this.y += this.dBear * yDir;
     this.display();
@@ -25,9 +26,9 @@ function Bear() {
     let t = parent.offsetTop;
     let w = parent.offsetWidth;
     let h = parent.offsetHeight;
-    if (this.x < 0) this.x = 0;
+    if (this.x <= 0) this.x = 1200;
     if (this.x > w - iw) this.x = w - iw;
-    if (this.y < 0) this.y = 0;
+    if (this.y <= 0) this.y = 0;
     if (this.y > h - ih) this.y = h - ih;
   };
 }
@@ -61,13 +62,6 @@ function moveBear(e) {
   if (e.keyCode == KEYDOWN) {
     bear.move(0, 1);
   } // down key
-
-  this.move = function (xDir, yDir) {
-    this.fitBounds(); //we add this instruction to keep bear within board
-    this.x += this.dBear * xDir;
-    this.y += this.dBear * yDir;
-    this.display();
-  };
 }
 
 class Bee {
@@ -111,5 +105,51 @@ class Bee {
       if (this.y < 0) this.y = 0;
       if (this.y > h - ih) this.y = h - ih;
     };
+  }
+}
+
+function createBeeImg(wNum) {
+  //get dimension and position of board div
+  let boardDiv = document.getElementById("board");
+  let boardDivW = boardDiv.offsetWidth;
+  let boardDivH = boardDiv.offsetHeight;
+  let boardDivX = boardDiv.offsetLeft;
+  let boardDivY = boardDiv.offsetTop;
+  //create the IMG element
+  let img = document.createElement("img");
+  img.setAttribute("src", "images/bee.gif");
+  img.setAttribute("width", "100");
+  img.setAttribute("alt", "A bee!");
+  img.setAttribute("id", "bee" + wNum);
+  img.setAttribute("class", "bee"); //set class of html tag img
+  //add the IMG element to the DOM as a child of the board div
+  img.style.position = "absolute";
+  boardDiv.appendChild(img);
+  //set initial position
+  let x = getRandomInt(boardDivW);
+  let y = getRandomInt(boardDivH);
+  img.style.left = boardDivX + x + "px";
+  img.style.top = y + "px";
+  //return the img object
+  return img;
+}
+
+function makeBees() {
+  //get number of bees specified by the user
+  let nbBees = document.getElementById("nbBees").value;
+  nbBees = Number(nbBees); //try converting the content of the input to a number
+  if (isNaN(nbBees)) {
+    //check that the input field contains a valid number
+    window.alert("Invalid number of bees");
+    return;
+  }
+  //create bees
+  let i = 1;
+  while (i <= nbBees) {
+    var num = i;
+    var bee = new Bee(num); //create object and its IMG element
+    bee.display(); //display the bee
+    bees.push(bee); //add the bee object to the bees array
+    i++;
   }
 }
